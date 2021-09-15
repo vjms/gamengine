1a86f49586bb9f92dddffc910b829dfae2d6d905
Valtteri Sjöblom <walt.sjoblom@gmail.com>
2021-09-15 14:10:03 +0300

    added window class, and fought with cmake to make it compile

--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,8 @@
+#include "window/window.h"
 
-
-
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
-
-#include <fmt/format.h>
-
-#include <chrono>
-
-void error_callback(int error, const char *description)
-{
-	fmt::print(stderr, "Error Code{}: {}", error, description);
-}
+#include <memory>
 
 int main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[])
 {
-	if (!glfwInit())
-	{
-		fmt::print("GLFW initialization failed\n");
-		exit(1);
-	}
-	auto window = glfwCreateWindow(640, 480, "Test", NULL, NULL);
-	glfwMakeContextCurrent(window);
-	glfwSetErrorCallback(error_callback);
-
-	if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
-		fmt::print("GLAD initialization failed\n");
-		exit(1);
-	}
-
-	while (!glfwWindowShouldClose(window))
-	{
-		// auto begin = std::chrono::high_resolution_clock::now();
-		glfwSwapBuffers(window);
-		glfwPollEvents();
-		// auto end = std::chrono::high_resolution_clock::now();
-		//fmt::print("fps {}\n", 1000000 / static_cast<double>(std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count()));
-	}
-	glfwDestroyWindow(window);
-	glfwTerminate();
-	return 0;
+	auto window = std::make_unique<Window>();
 }
-
-
-
